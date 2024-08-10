b175ef3dbb40d93f7508454d38161b8cf70205c4
fardinariful <fardinewucse2020@gmail.com>
2024-08-10 18:14:03 +0600

    js code added

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+function playnow(){
+    const homesection=document.getElementById('home-screen');
+    homesection.classList.add('hidden');
+
+    const playgrounds=document.getElementById('playground');
+    playgrounds.classList.remove('hidden');
+}
+
